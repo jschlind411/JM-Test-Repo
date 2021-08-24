bd24a5298379f2389bae916c78b46a192ec4cee7
Jeff S <jeff.schlindwein@gmail.com>
2021-08-24 15:18:49 -0700

    Finished initial fizzbuzzer create test, modified to check for counts.

--- FizzBuzz1.0/FizzBuzz Tester/UnitTest1.cs
+++ FizzBuzz1.0/FizzBuzz Tester/UnitTest1.cs
@@ -1,15 +1,18 @@
 using System;
 using Xunit;
+using FizzBuzz;
 
 namespace FizzBuzz_Tester
 {
     public class UnitTest1
     {
         [Fact]
-        public void CanCreateFizzBuzzer()
+        public void CanCreateFizzBuzzer_WithCorrectDefaults()
         {
             var fizzBuzzer = new FizzBuzzer();
-           
+
+            Assert.NotNull(fizzBuzzer);
+            Assert.Equal(0, fizzBuzzer.FizzCount);
         }
     }
 }
